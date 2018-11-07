4a4ec87fffea5e7b21b11752f176e8e9f78faf5a
wangxia/work <xiawang1024@icloud.com>
2018-11-07 17:00:24 +0800

    feat:deploy pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   apps: [
     {
-      name: 'API',
+      name: 'pm2_deploy',
       script: 'app.js',
 
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
       // args: 'one two',
-      instances: 'max',
+      instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
@@ -22,10 +22,10 @@ module.exports = {
   deploy: {
     production: {
       user: 'xiawang1024',
-      host: 'https://github.com',
+      host: '35.194.208.201',
       ref: 'origin/master',
-      repo: 'https://github.com/xiawang1024/pm2.git',
-      path: '/var/www/production',
+      repo: 'git@github.com:xiawang1024/pm2.git',
+      path: '/home/xiawang1024/work/pm2',
       'post-deploy':
         'npm install && pm2 reload ecosystem.config.js --env production'
     }
