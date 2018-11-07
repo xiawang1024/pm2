933be8734810a4c4e4e9e360e4827b33786c7e19
wangxia/work <xiawang1024@icloud.com>
2018-11-07 17:35:36 +0800

    feat:path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -26,7 +26,7 @@ module.exports = {
       host: '35.194.208.201',
       ref: 'origin/master',
       repo: 'git@github.com:xiawang1024/pm2.git',
-      path: '/home/xiawang1024/work/pm2',
+      path: '/home/xiawang1024/work/production',
       'post-deploy':
         'npm install && pm2 reload ecosystem.config.js --env production'
     }
